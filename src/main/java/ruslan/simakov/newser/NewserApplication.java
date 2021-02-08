319cfb943d7c527f41dfcfb9a07cef72b72199b9
RUSLAN SIMAKOV <ua667766706@gmail.com>
2021-02-08 22:35:40 +0200

    first commit

---
+++ src/main/java/ruslan/simakov/newser/NewserApplication.java
@@ -0,0 +1,13 @@
+package ruslan.simakov.newser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewserApplication.class, args);
+    }
+
+}
