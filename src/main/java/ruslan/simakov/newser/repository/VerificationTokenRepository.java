1553c4ea3b20402bb7ab59d414c793557f9e9a69
RUSLAN SIMAKOV <ua667766706@gmail.com>
2021-02-12 09:15:02 +0200

    Added database settings

--- src/main/java/ruslan/simakov/newser/repository/VerificationTokenRepository.java
+++ src/main/java/ruslan/simakov/newser/repository/VerificationTokenRepository.java
@@ -2,7 +2,8 @@ package ruslan.simakov.newser.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ruslan.simakov.newser.model.VerificationToken;
 
 @Repository
-public interface VerificationTokenRepository extends JpaRepository<VerificationTokenRepository, Long> {
+public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long> {
 }
--- src/main/java/ruslan/simakov/newser/repository/VoteRepository.java
+++ src/main/java/ruslan/simakov/newser/repository/VoteRepository.java
@@ -2,7 +2,8 @@ package ruslan.simakov.newser.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import ruslan.simakov.newser.model.Vote;
 
 @Repository
-public interface VoteRepository extends JpaRepository<VoteRepository, Long> {
+public interface VoteRepository extends JpaRepository<Vote, Long> {
 }
