44ab41ee817ebf4aa6b96e6e8d27797e217cd672
RUSLAN SIMAKOV <ua667766706@gmail.com>
2021-02-13 14:49:07 +0200

    Added account activation

---
+++ src/main/java/ruslan/simakov/newser/exeption/SpringUserNotFoundException.java
@@ -0,0 +1,6 @@
+package ruslan.simakov.newser.exeption;
+
+public class SpringUserNotFoundException extends X {
+    public SpringUserNotFoundException(String s) {
+    }
+}
---
+++ src/main/java/ruslan/simakov/newser/exeption/SpringVerificationTokenException.java
@@ -0,0 +1,6 @@
+package ruslan.simakov.newser.exeption;
+
+public class SpringVerificationTokenException {
+    public SpringVerificationTokenException(String s) {
+    }
+}
